e65245ff9a4e9e4a3cf0c67e8b1bcc846122e2c2
adityarajsingh9415 <adityaraj.iimt@gmail.com>
2024-08-07 17:29:03 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello GameMano, this is a simple node application!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
